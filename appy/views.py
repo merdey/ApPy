2424b009ab63837eadd11d634b492d705146f2d6
merdey <michaelerdey@gmail.com>
2015-07-30 00:44:05 -0700

    Fill in create_application view

--- appy/views.py
+++ appy/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from appy.models import Application
@@ -10,4 +11,20 @@ def list_applications(request):
     return render(request, 'list.html', {'apps': apps})
 
 def create_application(request):
-    pass
+    company = request.POST.get('company', '')
+    position = request.POST.get('position', '')
+    status = request.POST.get('status', '')
+    link = request.POST.get('link', '')
+
+    try:
+        app = Application(
+            company=company,
+            position=position,
+            status=status,
+            link=link,
+        )
+        app.save()
+
+        return JsonResponse({'success': True})
+    except Exception:
+        return JsonResponse({'success': False})
