ba9ecde29ce85667d4944889a6a0d23e19de37e3
merdey <michaelerdey@gmail.com>
2015-07-29 23:46:11 -0700

    Add link field to Application model

--- appy/models.py
+++ appy/models.py
@@ -5,6 +5,7 @@ class Application(models.Model):
     position = models.TextField()
 
     status = models.TextField()
+    link = models.TextField(blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
