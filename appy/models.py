0fd453bed2756f400ddfd716b171b9ec8cf775aa
merdey <michaelerdey@gmail.com>
2015-10-14 22:05:48 -0700

    Redesign of models

--- appy/models.py
+++ appy/models.py
@@ -1,11 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Application(models.Model):
+
+class Tag(models.Model):
+    description = models.TextField()
+
+
+class Position(models.Model):
     company = models.TextField()
     position = models.TextField()
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag)
 
-    status = models.TextField()
-    link = models.TextField(blank=True)
+
+class Application(models.Model):
+    user = models.ForeignKey(User)
+    position = models.ForeignKey(Position)
+
+    APPLIED = 'APP'
+    REJECTED = 'REJ'
+    INTERVIEWING = 'INT'
+    NEGOTIATING = 'NEG'
+    STATUS_CHOICES = (
+        (APPLIED, 'Applied'),
+        (REJECTED, 'Rejected'),
+        (INTERVIEWING, 'Interviewing'),
+        (NEGOTIATING, 'Negotiating'),
+    )
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default=APPLIED)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
