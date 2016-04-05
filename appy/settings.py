ccc55c776cebd79c1963bed8169aca4c7670c1e7
merdey <michaelerdey@gmail.com>
2016-04-04 23:22:21 -0700

    Keep secrets secret

--- appy/settings.py
+++ appy/settings.py
@@ -13,14 +13,13 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+from appy.settings_secret import *
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=dwsc71)0^vl3s-@t_0h9rlj84r-642ez+as99n^@8j%-7cwpd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
